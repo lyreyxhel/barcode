5e2c0f554d4416b819f988cab1ff1e3dbea41075
Fritz <160713554+lyreyxhel@users.noreply.github.com>
2025-06-21 22:46:00 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="text-center my-auto py-2">
+            <span>© 2025 Barcode Attendance System - Developed by Fritz Ria Lyreyzhel Casemero</span>
+        </div>
+    </div>
+</footer>
